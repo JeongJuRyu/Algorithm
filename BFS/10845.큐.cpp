9b1b4b8f18468154cc1b1b99ac1deb3c57b5c654
jeongjuryu <totw2018@naver.com>
2020-12-15 07:11:45 +0900

    algorithm

---
+++ BFS/10845.큐.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	queue<int> q;
+	int temp;
+	while (t--) {
+		string s;
+		cin >> s;
+		if (s == "push") {
+			cin >> temp;
+			q.push(temp);
+		}
+		else if (s == "pop") {
+			if (q.empty()) cout << -1 << '\n';
+			else {
+				temp = q.front();
+				cout << temp << '\n';
+				q.pop();
+			}
+		}
+		else if (s == "size") {
+			cout << q.size() << '\n';
+		}
+		else if (s == "empty") {
+			if (q.empty()) cout << 1 << '\n';
+			else cout << 0 << '\n';
+		}
+		else if (s == "front") {
+			if (q.empty()) cout << -1 << '\n';
+			else cout << q.front() << '\n';
+		}
+		else if (s == "back") {
+			if (q.empty()) cout << -1 << '\n';
+			else cout << q.back() << '\n';
+		}
+	}
+	return 0;
+}
