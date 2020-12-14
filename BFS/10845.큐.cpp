0b5d8e7e432c180934c02a013208cb7a8ea48f9f
jeongjuryu <totw2018@naver.com>
2020-12-15 07:11:45 +0900

    algorithm

--- BFS/10845.큐.cpp
+++ BFS/10845.큐.cpp
@@ -15,6 +15,7 @@ int main() {
 			cin >> temp;
 			q.push(temp);
 		}
+
 		else if (s == "pop") {
 			if (q.empty()) cout << -1 << '\n';
 			else {
@@ -23,17 +24,21 @@ int main() {
 				q.pop();
 			}
 		}
+
 		else if (s == "size") {
 			cout << q.size() << '\n';
 		}
+
 		else if (s == "empty") {
 			if (q.empty()) cout << 1 << '\n';
 			else cout << 0 << '\n';
 		}
+
 		else if (s == "front") {
 			if (q.empty()) cout << -1 << '\n';
 			else cout << q.front() << '\n';
 		}
+
 		else if (s == "back") {
 			if (q.empty()) cout << -1 << '\n';
 			else cout << q.back() << '\n';
