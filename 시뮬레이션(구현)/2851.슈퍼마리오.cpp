d56ce065b67f345c6f1eb1e7999ff45d7f5c2ca1
jeongjuryu <totw2018@naver.com>
2021-05-16 12:44:56 +0900

    algorithm

--- 시뮬레이션(구현)/2851.슈퍼마리오.cpp
+++ 시뮬레이션(구현)/2851.슈퍼마리오.cpp
@@ -1,27 +1,13 @@
-/*단순 시뮬레이션 문제*/
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 int main() {
-	int sum[10];
-	vector<int> v(10);
-	for (int i = 0; i < 10; i++) {
-		cin >> v[i];
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++) cout << ' ';
+		for (int j = 0; j < (2 * n) - (2 * i) + 1; j++) cout << '*';
+		for (int j = 0; j < i; j++) cout << ' ';
+		cout << '\n';
 	}
-	int sum_of_mash = 0;
-	for (int i = 0; i < 10; i++) {
-		sum_of_mash += v[i];
-		sum[i] = sum_of_mash - 100;
-	}
-	int answer_index = 0;
-	int min_abs = abs(sum[0]);
-	for (int i = 1; i < 10; i++) {
-		if (min_abs >= abs(sum[i])) {
-			min_abs = abs(sum[i]);
-			answer_index = i;
-		}
-	}
-	cout << sum[answer_index] + 100 << '\n';
 	return 0;
 }
